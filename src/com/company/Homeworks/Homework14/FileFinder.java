317bcd39577d7ffde6d712618a9c542cb17a9fdf
Roman <ezgg2000@gmail.com>
2021-01-24 01:02:11 +0200

    fix

--- src/com/company/Homeworks/Homework14/FileFinder.java
+++ src/com/company/Homeworks/Homework14/FileFinder.java
@@ -9,9 +9,6 @@ public class FileFinder {
     public File findAbsoluteFile(File dir, String fileName) {
         dir = dir.getAbsoluteFile();
         File[] files = dir.listFiles();
-        if (files.length == 0) { // если папка пустая то выходим
-            return null;
-        }
         for (int i = 0; i < files.length; i++) {
             if (files[i].getName().equals(fileName)) { //нашли папку или файл с нужным именем
                 return files[i].getAbsoluteFile();
